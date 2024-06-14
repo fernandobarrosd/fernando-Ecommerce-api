faeaf994469891a214af35b3aad2ea84bbd08295
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-14 12:22:19 -0300

    Add setter method to password field in the CreateAdminRequest

--- src/main/java/com/fernando/fernando_ecommerce_api/requests/CreateAdminRequest.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/requests/CreateAdminRequest.java
@@ -1,10 +1,12 @@
 package com.fernando.fernando_ecommerce_api.requests;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 public class CreateAdminRequest {
     private String name;
     private String email;
+    @Setter
     private String password;
 }
