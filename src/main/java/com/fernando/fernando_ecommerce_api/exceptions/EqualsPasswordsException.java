8ea647202e4e8843f5086470a681e12f70598434
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-14 14:21:44 -0300

    Create EqualsPasswordsException

---
+++ src/main/java/com/fernando/fernando_ecommerce_api/exceptions/EqualsPasswordsException.java
@@ -0,0 +1,7 @@
+package com.fernando.fernando_ecommerce_api.exceptions;
+
+public class EqualsPasswordsException extends RuntimeException {
+    public EqualsPasswordsException(String message) {
+        super(message);
+    }   
+}
