e529de16b103f56aa1a81c7a558eb8ed0f98ef4a
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-14 15:04:33 -0300

    Create ProductService

---
+++ src/main/java/com/fernando/fernando_ecommerce_api/services/ProductService.java
@@ -0,0 +1,61 @@
+package com.fernando.fernando_ecommerce_api.services;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fernando.fernando_ecommerce_api.exceptions.EntityNotFoundException;
+import com.fernando.fernando_ecommerce_api.models.Product;
+import com.fernando.fernando_ecommerce_api.repositories.ProductRepository;
+import com.fernando.fernando_ecommerce_api.requests.CreateProductRequest;
+import com.fernando.fernando_ecommerce_api.responses.ProductResponse;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+    
+    public ProductResponse saveProduct(CreateProductRequest productRequest) {
+        Product product = new Product(productRequest);
+        productRepository.save(product);
+        return new ProductResponse(product);
+    }
+
+    public void deleteProduct(Integer productID) {
+        if (productRepository.findById(productID).isEmpty()) {
+            throw new EntityNotFoundException("This product is not exists".formatted(productID));
+        }
+        productRepository.deleteById(productID);
+    }
+
+    @Transactional
+    public ProductResponse updateUnitPrice(Integer productID, Double newUnitPrice) {
+        if (productRepository.findById(productID).isEmpty()) {
+            throw new EntityNotFoundException("This product is not exists".formatted(productID));
+        }
+        Product product = productRepository.findById(productID).get();
+        productRepository.updateUnitPrice(productID, newUnitPrice);
+        product.setUnitPrice(newUnitPrice);
+        return new ProductResponse(product);
+    }
+
+    public List<ProductResponse> findAll() {
+        return productRepository
+                        .findAll()
+                        .stream()
+                        .map(ProductResponse::new)
+                        .toList();
+
+    }
+
+    public ProductResponse findById(Integer productID) {
+        Optional<Product> productOptional = productRepository.findById(productID);
+
+        if (productOptional.isEmpty()) {
+            throw new EntityNotFoundException("This product is not exists".formatted(productID));
+        }
+        return new ProductResponse(productOptional.get());
+    } 
+}
