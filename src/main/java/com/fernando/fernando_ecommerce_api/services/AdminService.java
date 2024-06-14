e319b058313277d267f4d7026cea615e6220c8e0
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-14 14:21:54 -0300

    Update AdminService

--- src/main/java/com/fernando/fernando_ecommerce_api/services/AdminService.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/services/AdminService.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.fernando.fernando_ecommerce_api.exceptions.EntityAlreadyExists;
 import com.fernando.fernando_ecommerce_api.exceptions.EntityNotFoundException;
+import com.fernando.fernando_ecommerce_api.exceptions.EqualsPasswordsException;
 import com.fernando.fernando_ecommerce_api.models.Admin;
 import com.fernando.fernando_ecommerce_api.repositories.AdminRepository;
+import com.fernando.fernando_ecommerce_api.requests.CreateAdminRequest;
+import com.fernando.fernando_ecommerce_api.responses.AdminResponse;
 
 @Service
 public class AdminService {
@@ -19,31 +21,38 @@ public class AdminService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public Admin saveAdmin(Admin admin) {
-        Optional<Admin> adminByEmail = adminRepository.findByEmail(admin.getEmail());
-        if (adminByEmail.isPresent()) {
-            throw new EntityAlreadyExists("Admin %s email is already exists".formatted(admin.getEmail()));
+    public AdminResponse saveAdmin(CreateAdminRequest createAdminRequest) {
+        if (adminRepository.findByEmail(createAdminRequest.getEmail()).isPresent()) {
+            throw new EntityAlreadyExists("Admin %s email is already exists".formatted(createAdminRequest.getEmail()));
         }
-        String passwordEncoded = passwordEncoder.encode(admin.getPassword());
-        admin.setPassword(passwordEncoded);
+        String passwordEncoded = passwordEncoder.encode(createAdminRequest.getPassword());
+        createAdminRequest.setPassword(passwordEncoded);
+        Admin admin = new Admin(createAdminRequest);
         adminRepository.save(admin);
-        return admin;
+        return new AdminResponse(admin);
     }
 
     @Transactional
-    public Admin updatePassword(Integer id, String newPassword) {
-        Admin admin = findById(id);
-        String passwordEncoded = passwordEncoder.encode(newPassword);
-        adminRepository.updatePassword(id, passwordEncoded);
-        admin.setPassword(passwordEncoded);
-        return admin;
-    }
-
-    public Admin findById(Integer id) {
+    public void updatePassword(Integer id, String newPassword) {
         Optional<Admin> adminOptional = adminRepository.findById(id);
         if (adminOptional.isEmpty()) {
             throw new EntityNotFoundException("Admin is not exists");
         }
-        return adminOptional.get();
+        Admin admin = adminOptional.get();
+        String currentEncodedPassword = admin.getPassword();
+        
+        if (passwordEncoder.matches(newPassword, currentEncodedPassword)) {
+            throw new EqualsPasswordsException("This %s password is already registered".formatted(newPassword));
+        }
+        String passwordEncoded = passwordEncoder.encode(newPassword);
+        adminRepository.updatePassword(id, passwordEncoded);
+    }
+
+    public AdminResponse findById(Integer id) {
+        Optional<Admin> adminOptional = adminRepository.findById(id);
+        if (adminOptional.isEmpty()) {
+            throw new EntityNotFoundException("Admin is not exists");
+        }
+        return new AdminResponse(adminOptional.get());
     }
 }
