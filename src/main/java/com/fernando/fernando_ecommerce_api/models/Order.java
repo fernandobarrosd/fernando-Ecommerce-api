f1df79e5bbc651e2d8e277701b4945f0e1a713a2
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-15 17:40:05 -0300

    Add Getter, Setter and AllArgsConstructor annotations in Order model

--- src/main/java/com/fernando/fernando_ecommerce_api/models/Order.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/models/Order.java
@@ -10,7 +10,11 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
 @Entity
 @Table(name = "order_table")
 public class Order {
