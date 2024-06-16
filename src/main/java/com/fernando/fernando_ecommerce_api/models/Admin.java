9cdfea7a7c93710875d78d44d009aae0782da960
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-16 19:33:29 -0300

    Remove all not used imports in Admin model

--- src/main/java/com/fernando/fernando_ecommerce_api/models/Admin.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/models/Admin.java
@@ -1,9 +1,5 @@
 package com.fernando.fernando_ecommerce_api.models;
 
-import java.util.Collection;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.UserDetails;
 import com.fernando.fernando_ecommerce_api.enums.UserRole;
 import com.fernando.fernando_ecommerce_api.requests.CreateAdminRequest;
 import jakarta.persistence.*;
