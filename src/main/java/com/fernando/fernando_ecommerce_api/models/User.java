c2e2004e096ea529307e69c5d7099e00905b0192
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-16 19:12:03 -0300

    Implements UserDetails interface in User model

--- src/main/java/com/fernando/fernando_ecommerce_api/models/User.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/models/User.java
@@ -1,8 +1,13 @@
 package com.fernando.fernando_ecommerce_api.models;
 
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
 import com.fernando.fernando_ecommerce_api.enums.UserRole;
-
 import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -17,7 +22,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @MappedSuperclass
-public abstract class User {
+public abstract class User implements UserDetails{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -32,5 +37,16 @@ public abstract class User {
     private String password;
 
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.createAuthorityList(getRole().getValue());
+    }
+
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
 }
