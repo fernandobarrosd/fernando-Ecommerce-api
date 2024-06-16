4b6d150402e2c9be89b938ffce5ecf358af5d9cd
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-16 19:33:39 -0300

    Remove all not used imports in Client model

--- src/main/java/com/fernando/fernando_ecommerce_api/models/Client.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/models/Client.java
@@ -2,14 +2,7 @@ package com.fernando.fernando_ecommerce_api.models;
 
 import java.time.LocalDate;
 import java.util.List;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.ArrayList;
-import java.util.Collection;
-
 import com.fernando.fernando_ecommerce_api.enums.UserRole;
 import com.fernando.fernando_ecommerce_api.requests.CreateClientRequest;
 import jakarta.persistence.Column;
