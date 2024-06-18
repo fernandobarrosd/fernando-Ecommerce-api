c1325be34f99a2386f89d4a4ab10ad094978737c
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-18 19:15:53 -0300

    Create ResponseError

---
+++ src/main/java/com/fernando/fernando_ecommerce_api/responses/error/ResponseError.java
@@ -0,0 +1,18 @@
+package com.fernando.fernando_ecommerce_api.responses.error;
+
+import lombok.Builder;
+
+@Builder
+public record ResponseError(
+    String message,
+    Integer statusCode,
+    String path) {
+        
+        public String toJson() {
+            return "{" + "\n" +
+                    "\t" + "\"message\": \"%s\"".formatted(message) + "\n" +
+                    "\t" + "\"statusCode\": %d".formatted(statusCode) + "\n" +
+                    "\t" + "\"path\": \"%s\"".formatted(path) + "\n" +
+                   "}";
+        }
+    }
