f53138d803204d735f6eacaf45dc458079bb37d9
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-18 19:09:46 -0300

    Rename CreateOrderRequest to OrderRequest and remove clientID field

--- src/main/java/com/fernando/fernando_ecommerce_api/requests/OrderRequest.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/requests/OrderRequest.java
@@ -1,14 +1,9 @@
 package com.fernando.fernando_ecommerce_api.requests;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record CreateOrderRequest(
-    @NotNull(message = "The clientID should be not null")
-    @Min(value = 1, message = "The clientID field should be greater that zero")
-    Integer clientID,
-
+public record OrderRequest(
     @NotNull(message = "The products should be not null")
     @Size(min = 1, message = "The products size should be greater that zero")
     String[] products) {}
