7cc8dd9325c8c2a24f4dc76ee69d120a452c950b
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-18 19:32:09 -0300

    Remove NotOrderOwnerException

--- src/main/java/com/fernando/fernando_ecommerce_api/exceptions/NotOrderOwerException.java
+++
@@ -1,7 +0,0 @@
-package com.fernando.fernando_ecommerce_api.exceptions;
-
-public class NotOrderOwerException extends RuntimeException {
-    public NotOrderOwerException(String message) {
-        super(message);
-    }
-}
