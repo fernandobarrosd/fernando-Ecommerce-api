8e3a2e131258e839983e18523f18f8a10a1bb772
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-19 16:36:24 -0300

    Add handleMethodArgumentNotValid exception handler method

--- src/main/java/com/fernando/fernando_ecommerce_api/exception_handlers/GlobalExceptionHandlers.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/exception_handlers/GlobalExceptionHandlers.java
@@ -1,18 +1,23 @@
 package com.fernando.fernando_ecommerce_api.exception_handlers;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import com.fernando.fernando_ecommerce_api.exceptions.EntityAlreadyExistsException;
 import com.fernando.fernando_ecommerce_api.exceptions.EntityNotFoundException;
 import com.fernando.fernando_ecommerce_api.responses.error.ResponseError;
+import com.fernando.fernando_ecommerce_api.responses.error.ResponseErrorWithFields;
 import jakarta.servlet.http.HttpServletRequest;
 
 
 @RestControllerAdvice
-public class GlobalExceptionHandlers extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandlers {
     @ExceptionHandler(EntityAlreadyExistsException.class)
     public ResponseEntity<ResponseError> handleEntityAlreadyExists(
         EntityAlreadyExistsException exception,
@@ -35,5 +40,30 @@ public class GlobalExceptionHandlers extends ResponseEntityExceptionHandler {
             .statusCode(HttpStatus.NOT_FOUND.value())
             .build();
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseErrorWithFields> handleMethodArgumentNotValid(
+        MethodArgumentNotValidException exception,
+        HttpServletRequest request
+    ) {
+        List<Map<String, String>> fields = exception.getFieldErrors()
+        .stream()
+        .map(this::convertToMap)
+        .toList();
+
+        ResponseErrorWithFields responseError = ResponseErrorWithFields.builder()
+            .path(request.getRequestURI())
+            .statusCode(HttpStatus.BAD_REQUEST.value())
+            .fields(fields)
+            .build();
+        return ResponseEntity.badRequest().body(responseError);
+    }
+
+    private Map<String, String> convertToMap(FieldError fieldError) {
+        Map<String, String> field = new HashMap<>();
+        field.put("name", fieldError.getField());
+        field.put("errorMessage", fieldError.getDefaultMessage());
+        return field;
     }
 }
