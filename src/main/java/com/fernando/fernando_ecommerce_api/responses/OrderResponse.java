966313d23efef5798303d4f85f323a1c1153eeed
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-22 17:18:14 -0300

    Update OrderRequest validations

--- src/main/java/com/fernando/fernando_ecommerce_api/responses/OrderResponse.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/responses/OrderResponse.java
@@ -1,5 +1,7 @@
 package com.fernando.fernando_ecommerce_api.responses;
 
+import org.springframework.format.annotation.NumberFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,6 +13,7 @@ import lombok.Setter;
 @Builder
 public class OrderResponse {
     private Integer id, clientID;
+    @NumberFormat
     private Double totalPrice;
     private ProductResponse[] products;
 }
