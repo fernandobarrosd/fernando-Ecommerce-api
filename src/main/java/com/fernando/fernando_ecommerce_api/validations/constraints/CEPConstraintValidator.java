d8a9adf7ba6afa58ee776c6efe8ccb7fe27cb047
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-22 19:19:08 -0300

    Add conditional in CEPConstraintValidator

--- src/main/java/com/fernando/fernando_ecommerce_api/validations/constraints/CEPConstraintValidator.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/validations/constraints/CEPConstraintValidator.java
@@ -6,10 +6,12 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class CEPConstraintValidator implements ConstraintValidator<CEP, String>{
-
     @Override
     public boolean isValid(String cep, ConstraintValidatorContext context) {
-        String cepPattern = "/\\d{5}-?\\d{3}/";
-        return cep.matches(cepPattern);
+        if (cep != null) {
+            String cepPattern = "/\\d{5}-?\\d{3}/";
+            return cep.matches(cepPattern);
+        }
+        return true;
     }
 }
