1cea9cd32e2036d675147da1942fc7655d00d675
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-22 17:18:05 -0300

    Update ProductRequest validations

--- src/main/java/com/fernando/fernando_ecommerce_api/requests/ProductRequest.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/requests/ProductRequest.java
@@ -1,18 +1,24 @@
 package com.fernando.fernando_ecommerce_api.requests;
 
+import com.fernando.fernando_ecommerce_api.validations.annotations.Name;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
 
 public record ProductRequest(
     @NotNull(message = "The title is required")
     @NotBlank(message = "The title should not be empty")
+    @Name(message = "The first word should be starts with uppercase letter and the other words starts with lowercase or uppercase")
     String title,
 
     @NotNull(message = "The title is required")
     @NotBlank(message = "The title should not be empty")
     String description,
 
-    
+    @NotNull(message = "The quantity is required")
+    @Min(value = 1, message = "The quantity should has min 1")
     Integer quantity,
+
+    @NotNull(message = "The unitPrice is required")
     Double unitPrice
 ) {}
