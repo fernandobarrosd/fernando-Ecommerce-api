91570251f5597dc35d76a42f22995cd22d9da1c0
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-22 16:18:59 -0300

    Move CEP annotation to annotations folder

--- src/main/java/com/fernando/fernando_ecommerce_api/validations/annotations/CEP.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/validations/annotations/CEP.java
@@ -1,9 +1,12 @@
-package com.fernando.fernando_ecommerce_api.validations;
+package com.fernando.fernando_ecommerce_api.validations.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+import com.fernando.fernando_ecommerce_api.validations.constraints.CEPConstraintValidator;
+
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
