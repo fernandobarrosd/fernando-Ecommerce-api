e4ff6de81732e816e5337b1cb6b83612adb41b53
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-22 16:15:54 -0300

    Add documentation in AuthController

--- src/main/java/com/fernando/fernando_ecommerce_api/controllers/AuthController.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/controllers/AuthController.java
@@ -10,11 +10,40 @@ import com.fernando.fernando_ecommerce_api.requests.LoginRequest;
 import com.fernando.fernando_ecommerce_api.responses.LoginResponse;
 import com.fernando.fernando_ecommerce_api.services.AuthService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
+@Tag(name = "Authentication")
 public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @Operation(summary = "Login admin or client")
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "200",
+            content = {
+                @Content(
+                    schema = @Schema(implementation = LoginResponse.class),
+                    mediaType = "application/json"
+                )
+            }
+        ),
+        @ApiResponse(
+            responseCode = "404",
+            content = {
+                @Content(
+                    schema = @Schema(implementation = LoginResponse.class),
+                    mediaType = "application/json"
+                )
+            }
+        )
+    })
     @PostMapping("/auth/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
         LoginResponse loginResponse = authService.login(loginRequest);
