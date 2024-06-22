fb71e4d759e9b55abcb379e954b6374d38ca83e8
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-22 16:17:56 -0300

    Update ProductOrderRequest validation

--- src/main/java/com/fernando/fernando_ecommerce_api/requests/ProductOrderRequest.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/requests/ProductOrderRequest.java
@@ -1,9 +1,9 @@
 package com.fernando.fernando_ecommerce_api.requests;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record ProductOrderRequest(
     @NotNull(message = "The productTitle is required")
-    @NotEmpty(message = "The productTitle not should be empty")
+    @NotBlank(message = "The productTitle not should be empty")
     String productTile) {}
