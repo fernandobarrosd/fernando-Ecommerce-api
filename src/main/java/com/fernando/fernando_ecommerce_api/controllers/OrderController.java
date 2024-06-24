91be1eaa6e75ea40580272cafe070fecbdcf356f
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-24 15:12:25 -0300

    Add CrossOrigin annotation in OrderController

--- src/main/java/com/fernando/fernando_ecommerce_api/controllers/OrderController.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/controllers/OrderController.java
@@ -7,18 +7,17 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.fernando.fernando_ecommerce_api.responses.OrderResponse;
 import com.fernando.fernando_ecommerce_api.responses.ProductResponse;
 import com.fernando.fernando_ecommerce_api.responses.error.ResponseError;
 import com.fernando.fernando_ecommerce_api.responses.error.ResponseErrorWithFields;
 import com.fernando.fernando_ecommerce_api.services.OrderService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -26,13 +25,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-
 import com.fernando.fernando_ecommerce_api.models.Client;
 import com.fernando.fernando_ecommerce_api.requests.OrderRequest;
 import com.fernando.fernando_ecommerce_api.requests.ProductOrderRequest;
 
 @RestController
 @RequestMapping("/orders")
+@CrossOrigin(origins = "*")
 @Tag(name = "Order")
 public class OrderController {
     @Autowired
