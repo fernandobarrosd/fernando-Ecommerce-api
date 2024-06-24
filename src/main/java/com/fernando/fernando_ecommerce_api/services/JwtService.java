f90aa6f73d3ee5c061b7fcb26e01404daadaa7be
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-24 14:53:46 -0300

    Create jwtSecretKey attribute with jwt.secret.key property

--- src/main/java/com/fernando/fernando_ecommerce_api/services/JwtService.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/services/JwtService.java
@@ -1,5 +1,6 @@
 package com.fernando.fernando_ecommerce_api.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -7,11 +8,14 @@ import com.fernando.fernando_ecommerce_api.exceptions.InvalidJWTTokenException;
 
 @Service
 public class JwtService {
+    @Value("${jwt.secret.key}")
+    private String jwtSecret;
+
     public String generateToken(String userEmail, String userRole) {
         return JWT.create()
         .withClaim("userRole", userRole)
         .withClaim("userEmail", userEmail)
-        .sign(Algorithm.HMAC256("secret"));
+        .sign(Algorithm.HMAC256(jwtSecret));
     }
 
     public String getEmail(String token) {
