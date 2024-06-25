93dd220d986dac17bd17cdda1c88d708aa3fcac0
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-25 15:47:52 -0300

    Remove OpenAPIDefinition annotation in FernandoEcommerceApiApplication main class

--- src/main/java/com/fernando/fernando_ecommerce_api/FernandoEcommerceApiApplication.java
+++ src/main/java/com/fernando/fernando_ecommerce_api/FernandoEcommerceApiApplication.java
@@ -2,11 +2,8 @@ package com.fernando.fernando_ecommerce_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
-@OpenAPIDefinition(servers = {@Server(url = "/", description = "URL Default")})
 public class FernandoEcommerceApiApplication {
 
 	public static void main(String[] args) {
